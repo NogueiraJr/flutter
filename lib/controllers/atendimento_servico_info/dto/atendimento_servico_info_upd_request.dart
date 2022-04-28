a24cb75f209694271b78facf3f159f6167abb2d0
Nogueira Jr <nogueirajr@hotmail.com>
2022-04-28 10:48:22 -0300

    Create atendimento_servico_info_upd_request.dart

---
+++ lib/controllers/atendimento_servico_info/dto/atendimento_servico_info_upd_request.dart
@@ -0,0 +1,46 @@
+import 'package:ocstapi/ocstapi.dart';
+
+class AtendimentoServicoInfoUpdRequest extends Serializable {
+  int id;
+  int usuarioId;
+  int atendimentoServicoId;
+  String descricao;
+  String tags;
+  String observacao;
+
+  @override
+  Map<String, dynamic> asMap() {
+    return {
+      'id': id,
+      'usuarioId': usuarioId,
+      'atendimentoServicoId': atendimentoServicoId,
+      'descricao': descricao,
+      'tags': tags,
+      'observacao': observacao,
+    };
+  }
+
+  @override
+  void readFromMap(Map<String, dynamic> object) {
+    id = object['id'] as int;
+    usuarioId = object['usuarioId'] as int;
+    atendimentoServicoId = object['atendimentoServicoId'] as int;
+    descricao = object['descricao'] as String;
+    tags = object['tags'] as String;
+    observacao = object['observacao'] as String;
+  }
+
+  Map<String, String> validate() {
+    final Map<String, String> validateResult = {};
+    if (id == null) {
+      validateResult['id'] = 'Id é necessário';
+    }
+    if (atendimentoServicoId == null) {
+      validateResult['atendimentoServicoId'] = 'Atendimento é necessário';
+    }
+    if (usuarioId == null) {
+      validateResult['usuarioId'] = 'Cliente é necessário';
+    }
+    return validateResult;
+  }
+}
