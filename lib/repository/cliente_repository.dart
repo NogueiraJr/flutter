a533ec5a3ba59354137f3971749fbe8348426e46
Nogueira Jr <nogueirajr@hotmail.com>
2022-11-14 10:10:56 -0300

    Create cliente_repository.dart

---
+++ lib/repository/cliente_repository.dart
@@ -0,0 +1,171 @@
+import 'package:ocstapi/controllers/cliente/dto/cliente_del_request.dart';
+import 'package:ocstapi/controllers/cliente/dto/cliente_ins_request.dart';
+import 'package:ocstapi/controllers/cliente/dto/cliente_upd_request.dart';
+import 'package:ocstapi/model/cliente_model.dart';
+import 'package:ocstapi/repository/usuario_repository.dart';
+
+import '../ocstapi.dart';
+
+class ClienteRepository {
+  ClienteRepository(this.context)
+      : usuarioRepository = UsuarioRepository(context);
+  final ManagedContext context;
+  final UsuarioRepository usuarioRepository;
+
+  Future<ClienteModel> clienteIns(ClienteInsRequest request) async {
+    try {
+      final usuario =
+          await usuarioRepository.consultarUsuarioPorId(request.usuarioId);
+      final model = ClienteModel();
+      model.usuario = usuario;
+      model.nome = request.nome;
+      model.celular = request.celular;
+      model.email = request.email;
+      model.tags = request.tags;
+      model.ativo = request.ativo;
+      return await context.insertObject(model);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<bool> clienteUpd(ClienteUpdRequest request) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..values.nome = request.nome
+        ..values.email = request.email
+        ..values.celular = request.celular
+        ..values.tags = request.tags
+        ..where((w) => w.id).equalTo(request.id)
+        ..where((w) => w.usuario.id).equalTo(request.usuarioId);
+      final ret = await query.update();
+      return ret.isNotEmpty;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<bool> clienteUpdTagsApenas(ClienteUpdRequest request) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..values.tags = request.tags
+        ..where((w) => w.id).equalTo(request.id)
+        ..where((w) => w.usuario.id).equalTo(request.usuarioId);
+      final ret = await query.update();
+      return ret.isNotEmpty;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<String> clienteDel(ClienteDelRequest request) async {
+    try {
+      final usuario =
+          await usuarioRepository.consultarUsuarioPorId(request.usuarioId);
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.id).equalTo(request.id)
+        ..where((w) => w.usuario.id).equalTo(usuario.id);
+      return await query.delete() > 0
+          ? 'Apaguei o Cliente com sucesso.'
+          : 'Não achei o Cliente para apagar.';
+    } catch (e) {
+      print(e);
+      return 'Problemas ao apagar o Cliente.';
+    }
+  }
+
+  Future<ClienteModel> obterClientePorId(int id) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.id).equalTo(id);
+      return await query.fetchOne();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterClientesPorIdUsuario(int idUsuario) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.usuario.id).equalTo(idUsuario);
+      return await query.fetch();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterClientesPorNome(String nome) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.nome).contains(nome);
+      return await query.fetch();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterClientesPorCelular(String celular) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.celular).contains(celular);
+      return await query.fetch();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterClientesPorEmail(String email) async {
+    try {
+      final query = Query<ClienteModel>(context)
+        ..where((w) => w.email).contains(email);
+      return await query.fetch();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterAmostraClientesDaEmpresa(
+      List<int> usuariosDaEmpresa, int qtdeAmostra) async {
+    try {
+      if (qtdeAmostra > 0) {
+        final query = Query<ClienteModel>(context)
+          ..join(object: (j) => j.usuario)
+          ..where((w) => w.usuario.id).oneOf(usuariosDaEmpresa)
+          ..fetchLimit = qtdeAmostra
+          ..sortBy((s) => s.id, QuerySortOrder.descending);
+        return await query.fetch();
+      } else {
+        final query = Query<ClienteModel>(context)
+          ..join(object: (j) => j.usuario)
+          ..where((w) => w.usuario.id).oneOf(usuariosDaEmpresa)
+          ..sortBy((s) => s.id, QuerySortOrder.descending);
+        return await query.fetch();
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<ClienteModel>> obterClientesDaEmpresaDoUsuario(
+      int idUsuario, List<int> idsUsuariosEmpresa) async {
+    try {
+      final idPai =
+          await usuarioRepository.obterIdProprietarioDaEmpresa(idUsuario);
+      idsUsuariosEmpresa.add(idPai); //Incluso o idPai, claro.
+
+      return obterAmostraClientesDaEmpresa(idsUsuariosEmpresa, 1000);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
