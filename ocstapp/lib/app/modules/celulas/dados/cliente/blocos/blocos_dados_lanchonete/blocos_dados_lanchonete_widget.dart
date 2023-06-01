e58444a00fa4be85d76580819f4fe72aacb67650
Nogueira Jr <nogueirajr@hotmail.com>
2023-06-01 10:55:30 -0300

    Create blocos_dados_lanchonete_widget.dart

---
+++ ocstapp/lib/app/modules/celulas/dados/cliente/blocos/blocos_dados_lanchonete/blocos_dados_lanchonete_widget.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+class BlocosDadosClienteLanchoneteWidget extends StatefulWidget {
+  final double left;
+  final double right;
+  final double top;
+  final double bottom;
+
+  const BlocosDadosClienteLanchoneteWidget(
+      {Key key, this.left, this.right, this.top, this.bottom})
+      : super(key: key);
+  @override
+  _BlocosDadosClienteLanchoneteWidgetState createState() =>
+      _BlocosDadosClienteLanchoneteWidgetState();
+}
+
+class _BlocosDadosClienteLanchoneteWidgetState
+    extends State<BlocosDadosClienteLanchoneteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          left: widget.left,
+          right: widget.right,
+          top: widget.top,
+          bottom: widget.bottom),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.green[200],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('4',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com pedidos nos próximos 5 dias',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.orange[300],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('5',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'compraram nos últimos 5 dias',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.yellow[300],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('7',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'não compram a mais de 30 dias',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.red[200],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('3',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'não compram a mais de 60 dias',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.red[700],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('8',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'não compram a mais de 90 dias',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
