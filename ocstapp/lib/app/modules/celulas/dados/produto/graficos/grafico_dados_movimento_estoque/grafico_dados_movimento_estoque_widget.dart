b847faccf9332c99956a1b103399decbf65ee74c
Nogueira Jr <nogueirajr@hotmail.com>
2023-11-14 11:34:18 -0300

    Create grafico_dados_movimento_estoque_widget.dart

---
+++ ocstapp/lib/app/modules/celulas/dados/produto/graficos/grafico_dados_movimento_estoque/grafico_dados_movimento_estoque_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GraficoDadosMovimentoEstoqueWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("textFinder"),
+    );
+  }
+}
