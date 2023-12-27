2c4cbd4dac506676990aea82360b2f16ceb177fd
Nogueira Jr <nogueirajr@hotmail.com>
2023-12-27 16:37:11 -0300

    Create blocos_dados_lanchonete_widget.dart

---
+++ ocstapp/lib/app/modules/celulas/dados/tag/blocos/blocos_dados_lanchonete/blocos_dados_lanchonete_widget.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+class BlocosDadosTagLanchoneteWidget extends StatefulWidget {
+  final double left;
+  final double right;
+  final double top;
+  final double bottom;
+
+  const BlocosDadosTagLanchoneteWidget(
+      {Key key, this.left, this.right, this.top, this.bottom})
+      : super(key: key);
+
+  @override
+  _BlocosDadosLanchoneteWidgetState createState() =>
+      _BlocosDadosLanchoneteWidgetState();
+}
+
+class _BlocosDadosLanchoneteWidgetState
+    extends State<BlocosDadosTagLanchoneteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          left: widget.left,
+          right: widget.right,
+          top: widget.top,
+          bottom: widget.bottom),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.red[200],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('27',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com produtos em falta no estoque',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.green[700],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('48',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com pedidos entregues esta semana',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.orange[300],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('12',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com produtos vendidos hoje',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.green[200],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('9',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com retorno de Clientes neste mês',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.red[700],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('2',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com notas fiscais canceladas',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                color: Colors.yellow[300],
+              ),
+              width: 90,
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Text('2',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    'com entregas de produtos vencidos',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
